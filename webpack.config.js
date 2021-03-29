7ed9a9824b586485d374256a5a35171fe5101510
benct <ben@tomlin.no>
2021-03-29 11:57:18 +0200

    Fix environment value definition

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
     },
     plugins: [
         new webpack.DefinePlugin({
-            'process.env.NODE_ENV': 'production',
+            'process.env.NODE_ENV': JSON.stringify('production'),
         }),
     ],
 };
